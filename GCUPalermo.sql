e796b78c5639adde481ed596f12d2b0a550328cf
Fernando Duarte <108553217+ZimaChina@users.noreply.github.com>
2022-09-17 21:40:15 -0300

    Update GCUPalermo.sql

--- GCUPalermo.sql
+++ GCUPalermo.sql
@@ -1,3 +1,16 @@
-select * 
-from palermo_tres_de_febrero
-order by fecha_de_inscripcion between '01-Sep-2020' and '01-Feb-2021' desc 
+select
+	names as nombre_y_apellido,
+	sexo,
+	0 as age,
+	correos_electronicos as email,
+	birth_dates as fecha_nacimiento,
+	universidad,
+	to_date(fecha_de_inscripcion , 'YY-Mon-DD')as fecha_de_inscripcion ,
+	careers as carrera,
+	codigo_postal,
+	null as locacion
+FROM 
+	public.palermo_tres_de_febrero
+where 
+	fecha_de_inscripcion  >='20-Sep-01'
+	and fecha_de_inscripcion  <='21-Feb-01'
