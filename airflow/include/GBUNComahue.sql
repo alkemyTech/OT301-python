76f884b0dbab4f126425e1288fa2bd367a69290d
Facundo Sanabria <facundo0708@gmail.com>
2022-09-18 16:45:59 -0300

    Sentencias SQL

---
+++ airflow/include/GBUNComahue.sql
@@ -0,0 +1,3 @@
+SELECT universidad as university, carrera as career, fecha_de_inscripcion as inscription_date, ' ' as first_name, name as last_name, sexo, -1 as age, codigo_postal as postal_code, ' ' as location, correo_electronico as email
+FROM public.flores_comahue
+WHERE universidad='UNIV. NACIONAL DEL COMAHUE' and TO_DATE('2021-02-21','YYYY-MM-DD') >= TO_DATE(fecha_de_inscripcion,'YYYY-MM-DD') and TO_DATE(fecha_de_inscripcion,'YYYY-MM-DD') >= TO_DATE('2020-09-01','YYYY-MM-DD');
