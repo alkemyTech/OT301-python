d9fbbcf5edf1857c63563d11441659d1ef8b2a1e
Nuriagi <nuriaagimenez@gmail.com>
2022-09-19 15:07:52 -0300

    create queries of UNMoron

---
+++ airflow/include/GBUNMoron.sql
@@ -0,0 +1,19 @@
+-- Código de consulta para la universidad de Morón
+
+SELECT 
+	fechaiscripccion as inscription_date,
+	universidad as university,
+	carrerra as career,
+	null as first_name,
+	nombrre as last_name,
+	sexo as gender,
+	null as age,
+	codgoposstal as postal_code,
+	null as location,
+	eemail as email 
+FROM 
+	public.moron_nacional_pampa
+WHERE
+	universidad like '%morón%' and
+	to_DATE(fechaiscripccion,'DD/MM/YYYY') >= to_DATE('01/09/2020','DD/MM/YYYY') and
+	to_DATE(fechaiscripccion,'DD/MM/YYYY') < to_DATE('01/02/2021','DD/MM/YYYY');
