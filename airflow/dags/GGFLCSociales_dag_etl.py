0927e92b7cb3346ba297dde174dc945e1d53bd96
Sebastian Cabrera <sebacabrera.96@hotmail.com>
2022-09-19 17:20:32 -0300

    Added two dags files.

---
+++ airflow/dags/GGFLCSociales_dag_etl.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+    'GGFLatinoamericanaCsSociales_dags',
+    description='Dag para la Facultad Latinoamericana de Ciencias Sociales',
+    schedule_interval='%@hourly',
+    start_date=datetime(2022, 1, 1),
+) as dag:
+    task_1=DummyOperator(task_id='GGFLatinoamericanaCsSociales')
+    task_2=DummyOperator(task_id='Proc_with_Pandas')
+    task_3=DummyOperator(task_id='Load_data_in_S3')
+
+task_1 >> task_2 >> task_3
