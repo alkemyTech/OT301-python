547ac566462289a5cceaf7652004b1227e4de44a
JuanMVicente <j_m_vicente@yahoo.com.ar>
2022-09-19 16:51:52 -0300

    OT301-33 Retries de grupo de universidades E

--- airflow/dags/GEUAbiertaInteramericana_dag_etl.py
+++ airflow/dags/GEUAbiertaInteramericana_dag_etl.py
@@ -1,9 +1,17 @@
-'''COMO: Analista de datos
+'''COMO:Analista de Datos
+
+OT 301-25
 QUIERO: Configurar un DAG, sin consultas, ni procesamiento
 PARA: Hacer un ETL para 2 universidades distintas.
 Criterios de aceptación: 
 Configurar el DAG para procese la Universidad Abierta Interamericana
-Documentar los operators que se deberían utilizar a futuro, teniendo en cuenta que se va a hacer dos consultas SQL (una para cada universidad), se van a procesar los datos con pandas y se van a cargar los datos en S3.  El DAG se debe ejecutar cada 1 hora, todos los días.'''
+Documentar los operators que se deberían utilizar a futuro, teniendo en cuenta que se va a hacer dos consultas SQL (una para cada universidad), se van a procesar los datos con pandas y se van a cargar los datos en S3.  El DAG se debe ejecutar cada 1 hora, todos los días.
+
+OT301-33
+QUIERO: Configurar los retries con la conexión al a base de datos
+PARA: poder intentar nuevamente si la base de datos me produce un error
+Criterios de aceptación: 
+Configurar el retry para las tareas del DAG de la Universidad Abierta Interamericana'''
 
 from datetime import timedelta, datetime
 
@@ -11,17 +19,26 @@ from airflow import DAG
 
 from airflow.operators.dummy import DummyOperator
 
+
+# Declare the dag arguments
+default_args = {
+    'owner': 'OT301',
+    'retries':5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
 # Functions to execute when using the DAGS, at this moment they are not called because the DummyOpertors do not allow it
 
-def extraction(university):
+def extraction():
     # Extraction of the required data from the university associated with the database
     pass
 
-def transformation(university):
+def transformation():
     # Processing of data associated with the university
     pass
 
-def load(university):
+def load():
     # data load corresponding to the university received as a parameter
     pass
 
