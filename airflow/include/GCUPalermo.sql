984b4c65b5eb5df4ba9c50c24134bf16d663f93a
ferduarte <ferduarte@live.com.ar>
2022-09-20 21:39:09 -0300

    Query Universidades grupo C

---
+++ airflow/include/GCUPalermo.sql
@@ -0,0 +1,16 @@
+select
+	names as nombre_y_apellido,
+	sexo,
+	0 as age,
+	correos_electronicos as email,
+	birth_dates as fecha_nacimiento,
+	universidad,
+	to_date(fecha_de_inscripcion , 'YY-Mon-DD')as fecha_de_inscripcion ,
+	careers as carrera,
+	codigo_postal,
+	null as locacion
+FROM 
+	public.palermo_tres_de_febrero
+where 
+	fecha_de_inscripcion  >='20-Sep-01'
+	and fecha_de_inscripcion  <='21-Feb-01'
