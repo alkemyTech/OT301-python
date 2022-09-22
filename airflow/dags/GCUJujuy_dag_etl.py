9fd572a39da682fa59c5f5448fe2a86714772b46
ferduarte <ferduarte@live.com.ar>
2022-09-22 14:51:29 -0300

    Configuracion DAGS Grupo C

---
+++ airflow/dags/GCUJujuy_dag_etl.py
@@ -0,0 +1,43 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+
+#Se utilizara PythonOperator para ejecutar las funciones de extraccion, transformacion y carga. 
+from airflow.operators.python import PythonOperator
+import logging 
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['ferduarte@live.com.ar'], 
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': '',                                #Configuracion de retries
+    'retry_delay': timedelta(minutes=5)
+}
+
+#Logs con mensajes de informacion sobre la tarea que se esta ejecutando
+def extraccion():
+    logging.info("Extrayendo datos")
+def transformacion():
+    logging.info("Transformando datos")
+def cargando():
+    logging.info("Guardando datos")   
+    
+with DAG(
+    'GCUJujuy_dag_etl.py',
+    default_args= default_args,
+    description= 'ETL Universidad Jujuy',
+    schedule_interval= timedelta (hours=1),       #Configuracion intervalo de ejecucion
+    start_date= datetime(2022-9-20), 
+    catchup=False,
+    template_searchpath='''direccion de extraccion sqls''', 
+    ) as dag:
+    
+    #Ejecucion de tareas
+    extraccion_task = ""
+    transformacion_task = ""
+    cargando_task = ""
+    
+    
+    extraccion_task >> transformacion_task >> cargando_task
+    
