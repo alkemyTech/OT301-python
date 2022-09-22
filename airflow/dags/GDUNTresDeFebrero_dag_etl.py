98312e3b2079ed26b8330dcf6655f951681b7468
Juan Manuel Maidana <108939684+MJuanM@users.noreply.github.com>
2022-09-22 13:45:00 -0300

    Configurar un DAG, grupo de universidades D (#28)
    Co-authored-by: Juan Manuel Maidana <mjuan@Juanma.localdomain>

---
+++ airflow/dags/GDUNTresDeFebrero_dag_etl.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from datetime import timedelta
+
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args={
+    'owner': 'Alkemy',
+    'start_date': datetime(2022, 9, 19)
+}
+
+with DAG(dag_id='UNTresDeFebrero_dag_etl',
+        description='Universidad Nacional Tres de Febrero proceso ETL',
+        start_date=datetime(2022,9,19),
+        schedule_interval=timedelta(hours=1),
+        default_args=default_args,
+) as dag:
+
+    # Extraccion de archivos desde base SQL
+    extract = DummyOperator(
+        task_id='extraccion'
+    )
+
+    # Transformacion con pandas
+    transform = DummyOperator(
+        task_id = 'transformacion'
+    )
+
+    # Carga de datos en S3
+    load = DummyOperator(
+        task_id = 'carga'
+    )
+
+extract >> transform >> load
