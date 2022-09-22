c5e107097f35bdd95a751f68de1e4f7d828ca579
Juan Martín Vicente <104178640+JuanMVicente@users.noreply.github.com>
2022-09-22 13:31:49 -0300

    OT301 33 Retries grupo de universidades E (#27)
    * OT-301-25 DAGs grupo de universidades E
    
    * OT301-33 Retries de grupo de universidades E
    
    * OT301-33 Retries Grupo de universidades E
    
    * OT301-33 Retries Dags
    
    * OT301-33 Retries en los DAG

--- airflow/dags/GEUNDeLaPampa_dag_etl.py
+++ airflow/dags/GEUNDeLaPampa_dag_etl.py
@@ -1,15 +1,36 @@
-'''COMO: Analista de datos
+'''COMO:Analista de Datos
+
+OT 301-25
 QUIERO: Configurar un DAG, sin consultas, ni procesamiento
 PARA: Hacer un ETL para 2 universidades distintas.
 Criterios de aceptación: 
 Configurar el DAG para procese la Universidad Nacional De La Pampa
-Documentar los operators que se deberían utilizar a futuro, teniendo en cuenta que se va a hacer dos consultas SQL (una para cada universidad), se van a procesar los datos con pandas y se van a cargar los datos en S3.  El DAG se debe ejecutar cada 1 hora, todos los días.'''
+Documentar los operators que se deberían utilizar a futuro, teniendo en cuenta que se va a hacer dos consultas SQL (una para cada universidad), se van a procesar los datos con pandas y se van a cargar los datos en S3.  El DAG se debe ejecutar cada 1 hora, todos los días.
+
+OT301-33
+QUIERO: Configurar los retries con la conexión al a base de datos
+PARA: poder intentar nuevamente si la base de datos me produce un error
+Criterios de aceptación: 
+Configurar el retry para las tareas del DAG de la Universidad Nacional De La Pampa'''
+
 
 from datetime import timedelta, datetime
 
 from airflow import DAG
 
 from airflow.operators.dummy import DummyOperator
+
+
+# Declare the dag arguments
+default_args = {
+    'owner': 'OT301',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries':5,
+    'retry_delay': timedelta(minutes=5),
+}
+
 
 # Functions to execute when using the DAGS, at this moment they are not called because the DummyOpertors do not allow it
 
