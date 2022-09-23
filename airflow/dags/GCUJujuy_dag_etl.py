5fc99bab297dd29c69cc403a25c3e3b178bc7d52
Fernando Duarte <108553217+ZimaChina@users.noreply.github.com>
2022-09-23 17:11:10 -0300

    Update GCUJujuy_dag_etl.py
    Actualizacion a DummyOperator

--- airflow/dags/GCUJujuy_dag_etl.py
+++ airflow/dags/GCUJujuy_dag_etl.py
@@ -1,8 +1,8 @@
 from datetime import timedelta, datetime
 from airflow import DAG
-
+from airflow.operators.dummy import DummyOperator
 #Se utilizara PythonOperator para ejecutar las funciones de extraccion, transformacion y carga. 
-from airflow.operators.python import PythonOperator
+#from airflow.operators.python import PythonOperator
 import logging 
 
 default_args = {
@@ -24,18 +24,18 @@ def cargando():
     logging.info("Guardando datos")   
     
 with DAG(
-    'GCUJujuy_dag_etl.py',
+    'GCUPalermo_dag_etl.py',
     default_args= default_args,
-    description= 'ETL Universidad Jujuy',
+    description= 'ETL Universidad Palermo',
     schedule_interval= timedelta (hours=1),       #Configuracion intervalo de ejecucion
     start_date= datetime.fromisoformat('2022-09-20'), 
     catchup=False
     ) as dag:
     
-    #Ejecucion de tareas
-    extraccion_task = PythonOperator(task_id= 'extraccion', python_callable= extraccion) 
-    transformacion_task = PythonOperator(task_id= 'transformacion', python_callable= transformacion)
-    cargando_task = PythonOperator(task_id= 'cargando', python_callable= cargando)
+#Ejecucion de tareas
+    extraccion_task = DummyOperator(task_id='extraccion')
+    transformacion_task = DummyOperator(task_id='transformacion')
+    cargando_task = DummyOperator(task_id='cargando')
     
     
     extraccion_task >> transformacion_task >> cargando_task
