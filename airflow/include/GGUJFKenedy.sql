be42538accb235556378641fa06f5b211f99c403
Sebastian Cabrera <sebacabrera.96@hotmail.com>
2022-09-26 18:05:56 -0300

    Upgraded .sql queries to match the required data for transformation

--- airflow/include/GGUJFKenedy.sql
+++ airflow/include/GGUJFKenedy.sql
@@ -2,15 +2,15 @@ select
 	universidades as university,
 	carreras as career,
 	fechas_de_inscripcion as inscription_date,
-	'' as first_name,
-	nombres as last_name,
+	split_part(nombres ,'-',1) as first_name,
+	split_part(nombres ,'-',2) as last_name,
 	sexo as gender,
-	'0' as age,
+	(
+		split_part(to_char(to_date(fechas_de_inscripcion ,'YY-Mon-DD'),'YY'),'-',1)::INT+'100' - 
+		split_part(to_char(to_date(fechas_nacimiento ,'YY-Mon-DD'),'YY'),'-',1)::INT
+	) as age,
 	codigos_postales as postal_code,
-	direcciones as location,
+	'' as location,
 	emails as email
-from uba_kenedy uk  
-where(
-	to_date(fechas_de_inscripcion,'YY-Mon-DD') between to_date('20-Sep-01','YY-Mon-DD') and to_date('21-Feb-01','YY-Mon-DD')
-	and universidades like '%kennedy%'
-	)
+	from uba_kenedy uk  
+	where fechas_de_inscripcion between '20-Sep-01' and '21-Feb-01'
