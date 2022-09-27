71512656a367b7d41e43206c75abe6424ac18c14
JuanMVicente <j_m_vicente@yahoo.com.ar>
2022-09-27 16:15:16 -0300

    OT301-77 Inicio del trabajo en S3

--- airflow/dags/GEUNDeLaPampa_dag_etl.py
+++ airflow/dags/GEUNDeLaPampa_dag_etl.py
@@ -46,6 +46,8 @@ from airflow.operators.python import PythonOperator
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+from airflow.hooks.S3_hook import S3Hook
+
 import pandas as pd
 
 import logging
@@ -55,6 +57,10 @@ from os import remove
 from pathlib import Path
 
 from os import remove
+
+from boto3.exceptions import S3UploadFailedError
+
+from botocore.exceptions import ClientError
 
 # declare global variables
 airflow_folder = Path(__file__).resolve().parent.parent
@@ -157,10 +163,19 @@ def transformation():
 
 
 # data load corresponding to the university received as a parameter
-def load():
+def load(filename: str, key: str, bucket_name: str) -> None:
     try:
-        # implementation of the function
+        hook = S3Hook(aws_conn_id='aws_s3_bucket')
+        hook.load_file(filename=filename, key=key,bucket_name=bucket_name)
         logging.info(f"{date.today().year}-{date.today().month}-{date.today().day} - Load - load done successfully")
+    except ValueError:
+        logging.warning('File could already exist in s3 bucket destination. Check it.')
+    except FileNotFoundError:
+        logging.warning('Could not find the file')
+    except S3UploadFailedError:
+        logging.error('Bucket destination does not exist. Please check its name either on aws or in the code.')
+    except ClientError:
+        logging.error('Error connecting to Bucket. Check for Admin->Connections->aws_s3_bucket Key and SecretKey loaded data.')
     except:
         logging.warning(f"{date.today().year}-{date.today().month}-{date.today().day} - Load - load was not performed correctly")
 
@@ -186,7 +201,16 @@ with DAG(
         python_callable=transformation
     )
     
-    load = DummyOperator(task_id='load')
+    load = PythonOperator(
+        task_id='load',
+        dag=dag,
+        python_callable=load,
+        op_kwargs={
+            'filename': f'{airflow_folder}/datasets/{university}_process.txt',
+            'key': f'{university}_process.txt',
+            'bucket_name': 'cohorte-septiembre-5efe33c6',
+        }
+    )
 
 
     extraction >> transformation >> load
