5172c7b5c525ed53d04cb9e7348695990c3ff565
Juan Martín Vicente <104178640+JuanMVicente@users.noreply.github.com>
2022-09-27 18:52:58 -0300

    OT301-57 Implementar el Python Operator para el procesamiento (merge rama anterior) (#50)
    * OT301-49 Implementar SQL Operator
    
    * OT301-49 Implementar SQL Operator
    
    * OT301-57 Implementar el Python Operator para el procesamiento
    
    * OT301-57 Implementar el Python Operator para el procesamiento
    
    * "OT301-49 Implementar SQL Operator corregido"
    
    Co-authored-by: fgpalacios <fernando.palacios@kunan.com.ar>

--- airflow/dags/GEUNDeLaPampa_dag_etl.py
+++ airflow/dags/GEUNDeLaPampa_dag_etl.py
@@ -28,7 +28,13 @@ OT301-49
 QUIERO: Implementar SQL Operator
 PARA: tomar los datos de las bases de datos en el DAG
 Criterios de aceptación: Configurar un Python Operators, para que extraiga información de la base de datos utilizando el .sql disponible en el repositorio base de la Universidad Nacional De La Pampa
-Dejar la información en un archivo .csv dentro de la carpeta datasets.'''
+Dejar la información en un archivo .csv dentro de la carpeta datasets.
+
+OT301-57
+QUIERO: Implementar el Python Operator
+PARA: procesar los datos obtenidos de la base de datos dentro del DAG
+Criterios de aceptación: 
+Configurar el Python Operator para que ejecute la función que procese los datos para la Universidad Nacional de La Pampa'''
 
 from datetime import timedelta, datetime, date
 
@@ -125,7 +131,13 @@ with DAG(
         dag=dag,
         python_callable=extraction
     )
-    transformation = DummyOperator(task_id='transformation')
+    
+    transformation = PythonOperator(
+        task_id='transformation',
+        dag=dag,
+        python_callable=transformation
+    )
+    
     load = DummyOperator(task_id='load')
 
 
