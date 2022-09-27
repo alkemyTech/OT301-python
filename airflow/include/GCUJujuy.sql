283b262fbf4791c65b6d848dae73b75bb56c7976
Fernando Duarte <108553217+ZimaChina@users.noreply.github.com>
2022-09-26 22:22:38 -0300

    Update GCUJujuy.sql
    Cambio en la consulta. La misma no permitia ejecutar el task del posterior DAG.

--- airflow/include/GCUJujuy.sql
+++ airflow/include/GCUJujuy.sql
@@ -1,18 +1,18 @@
-#Consulta SQL Universidad Jujuy
+--Consulta SQL Universidad Jujuy
 
-SELECT 
-	nombre as nombre_y_apellido,
-	sexo,
-	0 as age, 
-	email, 
-	to_date(birth_date, 'YYYY-MM-DD')as fecha_nacimiento, 
-	university as universidad, 
-	to_date(inscription_date, 'YYYY-MM-DD') as fecha_inscripcion,
-	career as carrera, 
-	"location" as locacion,
-	null as codigo_postal 
-FROM 
-	public.jujuy_utn
-where 
-	inscription_date  >='2020-09-01'
-	and inscription_date  <='2021-09-01'
+	SELECT 
+nombre as nombre_y_apellido,
+sexo,
+0 as age, 
+email, 
+to_date(birth_date, 'YYYY-MM-DD')as fecha_nacimiento, 
+university as universidad, 
+inscription_date as fecha_inscripcion,
+career as carrera, 
+"location" as locacion,
+-1 as codigo_postal 
+	FROM 
+public.jujuy_utn
+	where  
+to_DATE(inscription_date,'YYYY/MM/DD') >= to_DATE('01/09/2020','DD/MM/YYYY') and
+to_DATE(inscription_date,'YYYY/MM/DD') < to_DATE('01/02/2021','DD/MM/YYYY')
