683c33f1b398958d65efd48af2fc8f897386ac23
Tomas-vilte <88698264+Tomas-vilte@users.noreply.github.com>
2022-09-29 17:14:22 -0300

    Use an operator created by the community to upload the Universidad Del Cine file to S3 (#75)
    * Implementar SQL Operator para el grupo de universidades H
    
    * Implementar el Python Operator para el grupo de universidades H
    
    * Fix
    
    * mplementar el Python Operator para el grupo de universidades H
    
    * Crear una función Python con Pandas
    
    * Utilizar un operador creado por la comunidad para subir a S3 el archivo de Universidad De Buenos Aires
    
    * Utilizar un operador creado por la comunidad para subir a S3 el archivo de Universidad Del Cine
    
    Co-authored-by: Fernando Palacios <fernandogabrielpalacios@gmail.com>

--- airflow/dags/GHUNDelCine_dag_etl.py
+++ airflow/dags/GHUNDelCine_dag_etl.py
@@ -5,6 +5,8 @@ import logging as log
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from pathlib import Path
 import pandas as pd
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
 
 # Files
 dir = Path(__file__).resolve().parent.parent
@@ -194,12 +196,131 @@ def save_df_text(df):
     except OSError as e:
         log.error(f'Directory not found: {e}')
 
+    # Create a variable with the columns to use
+    columns = ['university', 'career',
+               'last_name', 'gender', 'location', 'email']
 
-def load():
+    # Create an iteration for the columns
+    for column in columns:
+        # convert the dataframe to lowercase
+        df[f'{column}'] = df[f'{column}'].str.lower()
+        # Replace the "-" in the dataframe with a space
+        df[f'{column}'] = df[f'{column}'].apply(lambda x: x.replace('-', ' '))
+
+    # Replace values ​​of 'm' and 'f' with 'male' and 'female' in the gender column
+    df['gender'] = df.gender.replace({'f': 'female', 'm': 'male'})
+
+    # We clean the last_name column so that we only have the names and surnames
+    abreviations = ["mrs\.", "mr\.", "dr\.", "ms\.", "md",
+                "dds", "dvm", "iii", "phd", "jr\.", "ii", "iv", "miss"]
+
+    df['last_name'] = df['last_name'].replace(
+        abreviations, value='', regex=True
+    )
+    # Separate first and last name from the Last_name column, add them to the df and remove the Last_name column
+    df = df.drop(['first_name'], axis=1)
+    df = df.loc[:,~df.columns.duplicated()]
+    return df
+
+def calculate_age(df: pd.DataFrame, file: str) -> pd.DataFrame:
+    """Create a column called 'age', make the difference between the 'inscription_date' columns
+    and 'birth_date' to get the age in days. With age in days makes a transformation
+    in years with the calculate function
+
+    Args:
+        df (pd.DataFrame): DataFrame
+        file (str): Take as argument to the CSV file
+
+    Returns:
+        pd.DataFrame: Transformed DataFrame
+    """
+
+    def calculate(diff_days):
+        """
+        Get days difference, determine if they are negative or positive.
+        If they are negative, increment 100 years and return the division between days and years (age).
+        If they are positive, it only returns the division
+        """
+        days = diff_days.days
+        if days < 0:
+            days += int(100 * 365.2425)
+
+        return int(days / 365.2425)
+    if file == 'GHUNDelCine':
+        df['inscription_date'] = pd.to_datetime(df.inscription_date, format='%d-%m-%Y')
+        df['birth_date'] = pd.to_datetime(df.birth_date, format='%d-%m-%Y')
+        df['age'] = df['inscription_date'] - df['birth_date']
+
+    elif file == 'GHUNDeBuenosAires':
+        df['inscription_date'] = pd.to_datetime(df.inscription_date)
+        df['birth_date'] = pd.to_datetime(df.birth_date, format='%y-%b-%d')
+        df['age'] = df['inscription_date'] - df['birth_date']
+
+    df['age'] = df['age'].apply(calculate)
+    return df
+
+
+
+
+def postal_code_or_location(df: pd.DataFrame, postal_code_or_location: str) -> pd.DataFrame:
+    """Receives a dataframe and a string, the string defines whether to
+        add location, or postal_code, it only accepts those two values.
+
+    Args:
+        df (pd.DataFrame): DataFrame
+        postal_code_or_location (str): location or postal_code
+
+    Returns:
+        pd.DataFrame: Return the dataframe
+    """
+    try:
+        log.info('Reading zip code file from assets folder')
+        open_data = pd.read_csv(f'{dir}/assets/codigos_postales.csv')
+    except OSError as e:
+        log.error(f'File not found {e}')
+
+    if postal_code_or_location == 'location':
+        # Obteniendo localidad con codigo_postal
+        open_data["localidad"] = open_data['localidad'].apply(lambda x: x.lower())
+        dict_cp = dict(zip(open_data['codigo_postal'], open_data['localidad']))
+        df['location'] = df['postal_code'].apply(lambda x: dict_cp[x])
+
+    elif postal_code_or_location == 'postal_code':
+        # Obteniendo codigo postal con localidad
+        open_data['localidad'] = open_data["localidad"].apply(lambda x: x.lower())
+        dict_cp = dict(zip(open_data['localidad'], open_data['codigo_postal']))
+        df['postal_code'] = df['location'].apply(lambda x: dict_cp[x])
+
+    return df
+
+
+def save_df_text(df):
+    """
+    Save the dataframe in txt format. in the datasets folder
+    """
+    try:
+        log.info('Saving txt file, in the datasets folder')
+        with open(f'{dir}/datasets/{file}_process.txt', 'w+') as f:
+            f.write(df.to_string())
+            f.close()
+    except OSError as e:
+        log.error(f'Directory not found: {e}')
+
+
+def load(filename:str, key:str, bucket_name:str):
     """
     Function that is responsible for uploading the data to amazon s3
     """
-    print('Uploaded to s3')
+    log.info(f'Uploading file to s3 {filename}')
+    hook = S3Hook('')
+    log.info('Uploading file')
+    hook.load_file(
+        filename=filename,
+        key=key,
+        bucket_name=bucket_name,
+        replace=True
+    )
+    log.info('File uploaded to s3 successfully')
 
 
 with DAG(
@@ -227,7 +348,12 @@ with DAG(
     t3 = PythonOperator(
         task_id='Uploading_to_s3',
         dag=dag,
-        python_callable=load
+        python_callable=load,
+        op_kwargs={
+            'filename':  f'{dir}/datasets/GHUNDeBuenosAires_process.txt',
+            'key': f'{dir}_process.txt',
+            'bucket_name': 'cohorte-septiembre-5efe33c6'
+        }
     )
 
 t1 >> t2 >> t3
