303d2095e5ea4b12ae5aa1f9ef3b23a639f7d220
Sebastian Cabrera <sebacabrera.96@hotmail.com>
2022-10-03 16:00:21 -0300

    Fixed Dynamic DAGs destination folder.

--- airflow/plugins/generador_GG.py
+++ airflow/plugins/generador_GG.py
@@ -2,8 +2,9 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 import os
 
-
 file_dir=os.path.dirname(os.path.abspath(__file__))
+airflow_path=os.path.abspath(os.path.join(file_dir, os.pardir))
+dags_path=airflow_path+'/dags/'
 env=Environment(loader=FileSystemLoader(file_dir))
 template=env.get_template('template_GG.jinja2')
 
@@ -11,5 +12,5 @@ for filename in os.listdir(file_dir):
     if filename.endswith('.yaml'):
         with open (f'{file_dir}/{filename}','r') as configfile:
             config=yaml.safe_load(configfile)
-            with open(f"{file_dir}/{config['dag_id']}_dag_etl.py","w") as f:
+            with open(f"{dags_path}/DD_{config['dag_id']}_dag_etl.py","w") as f:
                 f.write(template.render(config))
