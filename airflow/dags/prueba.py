f3ee3d0e7cb14c735a991b5db06321a9620949e5
Facundo Sanabria <facundo0708@gmail.com>
2022-10-03 00:16:01 -0300

    Config. Dag Dinámico

--- airflow/dags/prueba.py
+++
@@ -1,5 +0,0 @@
-import os
-#print(os.getcwd())
-#print(os.path.dirname(os.path.realpath(__file__)).replace('/dags',''))
-
-print(os.path.isfile(os.path.dirname(os.path.realpath(__file__)).replace('/dags','/datasets/GBUSalvador.csv')))
