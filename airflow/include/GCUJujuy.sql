6cf810f63fb383b4aab6d9acac93261682b3322f
Fernando Duarte <108553217+ZimaChina@users.noreply.github.com>
2022-10-05 02:59:17 -0300

    Update GCUJujuy.sql
    Cambios en fecha de inscripcion y se agrego fecha de nacimiento para uso posterior.

--- airflow/include/GCUJujuy.sql
+++ airflow/include/GCUJujuy.sql
@@ -1,19 +1,19 @@
 --Consulta SQL Universidad Jujuy
 
 	SELECT 
-nombre as nombre_y_apellido,
-sexo,
-0 as age, 
-email, 
-to_date(birth_date, 'YYYY-MM-DD')as fecha_nacimiento, 
-university as universidad, 
-inscription_date as fecha_inscripcion,
-career as carrera, 
-"location" as locacion,
--1 as codigo_postal 
+nombre as last_name,
+sexo as gender,
+null as age, 
+email,
+birth_date, 
+university, 
+inscription_date,
+career, 
+null as first_name,
+location,
+-1 as postal_code
 	FROM 
 public.jujuy_utn
 	where  
 to_DATE(inscription_date,'YYYY/MM/DD') >= to_DATE('01/09/2020','DD/MM/YYYY') and
 to_DATE(inscription_date,'YYYY/MM/DD') < to_DATE('01/02/2021','DD/MM/YYYY')
-
