d2ffc5fd7d093522a06219a5106a8ccbed22cd93
Sebastian Cabrera <sebacabrera.96@hotmail.com>
2022-10-20 22:39:35 -0300

    Added .py files for hadoop

--- bigdata/hadoopCabrera/mapper.py
+++ bigdata/hadoopCabrera/mapper.py
@@ -7,20 +7,9 @@ import sys
 import operator
 import os
 
-with open('/home/seba/docker-hadoop/posts.xml','r') as f:
-    posts=f.readlines()
-    f.close()
-
-for line in posts:#sys.stdin
+for line in sys.stdin:
     if line[:6] == '  <row':
         element=ET.fromstring(line)
         ids=(element.attrib['Id'])
         views=int(element.attrib['ViewCount'])
-        asdf={}
-        asdf[ids]=views
-        for key,view_count in asdf:
-            oficial=dict(dic)
-        ordered_list=sorted(oficial.items(), key=lambda x:x[1], reverse=True)
-        #top_10=views[0:10]
-        print(ordered_list)
-        #print ('%s\t%s' % (views,ids))
+        print ('%s\t%s' % (views,ids))
